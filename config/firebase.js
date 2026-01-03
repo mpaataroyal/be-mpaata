62b5a509d63bbda373fefcf216e54d1dc1c9207e
rumbiiha <rswaib@gmail.com>
2026-01-03 13:16:51 +0300

    fix deployment

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,12 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Adjust path as needed
+const admin = require("firebase-admin");
 
-// Check if app is already initialized to avoid errors
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
